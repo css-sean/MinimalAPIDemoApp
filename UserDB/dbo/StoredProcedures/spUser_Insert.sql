efcc37475f7975ccb353cdc76c5e86cb3b0d1be6
css-sean <sean@christiansteven.com>
2022-02-01 14:39:51 -0500

    Commiting all files the first time

---
+++ UserDB/dbo/StoredProcedures/spUser_Insert.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Insert]
+	
+	@FirstName NVARCHAR(50), 
+	@LastName NVARCHAR(100)
+AS
+begin
+	insert into dbo.[User] (FirstName, LastName) 
+	values (@FirstName, @LastName);
+end
---
+++ UserDB/dbo/StoredProcedures/spUser_Update.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Update]
+	@Id int, 
+	@FirstName NVARCHAR(50), 
+	@LastName NVARCHAR(100)
+AS
+begin
+	update dbo.[User]
+	set FirstName = @FirstName, LastName = @LastName
+	where Id = @Id;
+end
